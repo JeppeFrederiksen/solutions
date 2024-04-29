392ae68986a5455b6e63249c4dc3c89894cf216a
JeppeFrederiksen <63899@edu.campusvejle.dk>
2024-04-29 11:40:19 +0200

    færdig

---
+++ S0410_cars_exercise.py
@@ -0,0 +1,35 @@
+"""
+Opgave "Cars":
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Definer en funktion drive_car(), der udskriver en bils motorlyd (f.eks. "roooaar")
+
+I hovedprogrammet:
+    Definer variabler, som repræsenterer antallet af hjul og den maksimale hastighed for 2 forskellige biler
+    Udskriv disse egenskaber for begge biler
+    Kald derefter funktionen drive_car()
+
+Hvis du ikke har nogen idé om, hvordan du skal begynde, kan du åbne S0420_cars_help.py og starte derfra.
+Hvis du går i stå, kan du spørge google, de andre elever eller læreren (i denne rækkefølge).
+Hvis du stadig er gået i stå, skal du åbne S0430_cars_solution.py og sammenligne den med din løsning.
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Team-besked til din lærer: <filename> færdig
+Fortsæt derefter med den næste fil."""
+
+
+def drive_car():
+    print("vroom")
+
+
+car1_wheels = 4
+car2_wheels = 3
+car1_speed = 120
+car2_speed = 80
+
+print(f"Car 1. Maxspeed: {car1_speed}. Wheels: {car1_wheels}.")
+print(f"car 2. Maxspeed: {car2_speed}. Wheels: {car2_wheels}.")
+drive_car()
